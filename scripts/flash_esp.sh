fdbf34427ef0e1a6101080cd2447479e91a37fb2
Lukas Gehreke <lk.gehreke@gmail.com>
2026-04-09 11:24:19 +0200

    Added legacy mode to flash script

--- scripts/flash_esp.sh
+++ scripts/flash_esp.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 
 PORT=$1
-BOOTLOADER_FILE=$2
-PARTITION_FILE=$3
-IMAGE_FILE=$4
+MODE=$2
+BOOTLOADER_FILE=$3
+PARTITION_FILE=$4
+IMAGE_FILE=$5
+
+SPLITTER=""
+
+if [ $MODE = "NEW" ]; then
+      SPLITTER="-"
+elif [ $MODE = "LEGACY" ]; then
+      SPLITTER="_"
+else
+      echo "Mode needs to be NEW or LEGACY"
+      exit 1
+fi
 
 sh -c "esptool.py \
       --chip esp32 \
       --port ${PORT} \
       --baud 460800 \
-      --before default-reset write-flash \
+      --before default${SPLITTER}reset write${SPLITTER}flash \
       -z \
-      --flash-mode dout \
-      --flash-freq 40m \
-      --flash-size detect \
+      --flash${SPLITTER}mode dout \
+      --flash${SPLITTER}freq 40m \
+      --flash${SPLITTER}size detect \
       0x1000 ${BOOTLOADER_FILE} \
       0x8000 ${PARTITION_FILE} \
       0x10000 ${IMAGE_FILE}"
