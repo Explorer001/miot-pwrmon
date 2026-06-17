670b793b287ccb76c75b901eb9617a2789a6686e
Lukas Gehreke <lk.gehreke@gmail.com>
2026-06-17 16:44:03 +0200

    scripts: Added reference plot options to powerplot

--- scripts/pwrplot.py
+++ scripts/pwrplot.py
@@ -19,11 +19,98 @@ import argparse
 import pandas as pd
 import matplotlib.pyplot as plt
 
-PWRMON_HEADER_ROW=6
+PWRMON_HEADER_ROW_MIOT=6
+PWRMON_HEADER_ROW_PPK=0
 
-def process_file(file_name):
-    df = pd.read_csv(file_name, sep=';', index_col='timestamp' ,header=PWRMON_HEADER_ROW)
-    df['current_ua'].plot()
+def _load_file_miot(file_name: str) -> pd.DataFrame:
+    """
+    Load the power measurements from the miot power monitor.
+
+    file_name: Name of the file.
+
+    Returns a Dataframe containing the measurements.
+    """
+    df = pd.read_csv(file_name, sep=';' ,header=PWRMON_HEADER_ROW_MIOT)
+    df = df.rename(columns={'timestamp': 'timestamp_ms'})
+    
+    return df[['timestamp_ms', 'current_ua']]
+
+def _load_file_ppk(file_name: str) -> pd.DataFrame:
+    """
+    Load the power measurements from the power profiler kit.
+
+    file_name: Name of the file.
+
+    Returns a Dataframe containing the measurements.
+    """
+    df = pd.read_csv(file_name, sep=',' ,header=PWRMON_HEADER_ROW_PPK)
+    df = df.rename(columns={'Timestamp(ms)': 'timestamp_ms', 'Current(uA)': 'current_ua'})
+
+    return df[['timestamp_ms', 'current_ua']]
+
+def _load_file(file_name: str) -> pd.DataFrame:
+    """
+    Automatically detect file type and load power measurements.
+
+    file_name: Name of the file.
+
+    Returns a Dataframe containing the measurements.
+    """
+    with open(file_name, 'r') as f:
+        l = f.readline()
+        if '# Channels' in l:
+            is_miot = True
+        else:
+            is_miot = False
+
+    if is_miot:
+        return _load_file_miot(file_name)
+
+    return _load_file_ppk(file_name)
+
+def _get_first_spike_ts(meas: pd.DataFrame):
+    """
+    Get the timestamp of the first spike.
+
+    meas: The measurement dataframe.
+
+    Return the timestamp of the first spike.
+    """
+    max_current = meas['current_ua'].max()
+    avg_current = meas['current_ua'].mean()
+
+    threshold_current = avg_current + (max_current - avg_current) / 2
+
+    return meas[meas.current_ua > threshold_current].iloc[0]['timestamp_ms']
+
+def process_files(measurement_name: str, reference_name: str, sync: bool):
+    """
+    Plot the power measurements and reference.
+
+    measurement_name: The file name of the measurement file.
+    reference_name: The file name of the reference file.
+    sync: Synchronize measurements and reference on first spike.
+    """
+
+    meas = _load_file(measurement_name)
+
+    if reference_name != None:
+        plot_ref = True
+        ref = _load_file(reference_name)
+    else:
+        plot_ref = False
+
+    if sync and plot_ref:
+        meas_spike = _get_first_spike_ts(meas)
+        ref_spike = _get_first_spike_ts(ref)
+        diff = ref_spike - meas_spike
+        print(f'Adjusting reference by {diff} ms')
+
+        ref.timestamp_ms = ref.timestamp_ms - diff
+
+    ax = meas.plot(x='timestamp_ms', y='current_ua', xlabel='Timestamp (ms)', ylabel='Current (uA)', legend=False, color='tab:blue')
+    if plot_ref:
+        ref.plot(ax=ax, x='timestamp_ms', y='current_ua', xlabel='Timestamp (ms)', ylabel='Current (uA)', legend=False, color='tab:orange')
     plt.show()
         
 
@@ -33,6 +120,8 @@ if __name__ == '__main__':
         description='Plot bus voltage and current from file')
     
     parser.add_argument('file', help='Input file', type=str)
+    parser.add_argument('reference', help='Reference power measurement', type=str, nargs='?')
+    parser.add_argument('-s', '--sync', help='Synchronize file and reference on first power spike', action='store_true')
 
     args = parser.parse_args()
-    process_file(args.file)
+    process_files(args.file, args.reference, args.sync)
